003568982d7b46aa18ec276a188bffab7b06a2fa
mgaudre <mgaudre@PC2>
2025-02-05 13:45:19 +0100

    + Ajout du nom de l'application

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -30,176 +30,176 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFF323232),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          const SizedBox(height: 10),
-
-          Image.asset(
-            "assets/images/logo_t.png",
-            height: MediaQuery.of(context).size.width * 0.6,
-          ),
-
-          const SizedBox(height: 20),
-
-          const Text(
-              'Application pour serveurs',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  fontSize: 30,
-                  color: Color(0xFFFFEB99),
-                  fontFamily: 'LibreBaskerville'
-              )
-          ),
-
-          const SizedBox(height: 30),
-
-          Padding(
-            padding: const EdgeInsets.only(left: 10, right: 10),
-            child: GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => Commande(title: 'Commande', token: widget.token),
-                  ),
-                );
-              },
-              child: Container(
-                width: MediaQuery.of(context).size.width*0.9,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  border: Border.all(color: Color(0xFFFFEB99), width: 0.5),
-                  color: Color(0xFF292929),
-                ),
-                padding: const EdgeInsets.all(20),
-                child: const Column(
-                  children: [
-                    Text(
-                      'Prendre un commande',
-                      style: TextStyle(
-                        fontSize: 18,
-                        color: Color(0xFFFFEB99),
-                        fontFamily: 'LibreBaskerville'
-                      )
-                    ),
-                  ],
-                ),
-              ),
+      body: Center(
+        child:ListView(
+          shrinkWrap: true,
+          children: <Widget>[
+            Image.asset(
+              "assets/images/logo_t.png",
+              height: MediaQuery.of(context).size.width * 0.6,
             ),
-          ),
 
-          const SizedBox(height: 10),
-          Padding(
-            padding: const EdgeInsets.only(left: 10, right: 10),
-            child: GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => Scanner(title: 'Scanner', token: widget.token),
-                  ),
-                );
-              },
-              child: Container(
-                width: MediaQuery.of(context).size.width*0.9,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  border: Border.all(color: Color(0xFFFFEB99), width: 0.5),
-                  color: Color(0xFF292929),
-                ),
-                padding: const EdgeInsets.all(20),
-                child: const Column(
-                  children: [
-                    Text(
-                      'Scanner un réservation',
-                      style: TextStyle(
-                        fontSize: 18,
-                        color: Color(0xFFFFEB99),
-                        fontFamily: 'LibreBaskerville'
-                      )
+            const SizedBox(height: 20),
+
+            const Text(
+                'Application pour serveurs',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 30,
+                    color: Color(0xFFFFEB99),
+                    fontFamily: 'LibreBaskerville'
+                )
+            ),
+
+            const SizedBox(height: 30),
+
+            Padding(
+              padding: const EdgeInsets.only(left: 10, right: 10),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Commande(title: 'Commande', token: widget.token),
                     ),
-                  ],
-                ),
-              )
-            )
-          ),
-
-          const SizedBox(height: 10),
-
-          Padding(
-            padding: const EdgeInsets.only(left: 10, right: 10),
-            child: GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => Discussion(title: 'Discussion', token: widget.token),
+                  );
+                },
+                child: Container(
+                  width: MediaQuery.of(context).size.width*0.9,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(color: Color(0xFFFFEB99), width: 0.5),
+                    color: Color(0xFF292929),
                   ),
-                );
-              },
-              child: Container(
-                width: MediaQuery.of(context).size.width*0.9,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  border: Border.all(color: Color(0xFFFFEB99), width: 0.5),
-                  color: Color(0xFF292929),
-                ),
-                padding: const EdgeInsets.all(20),
-                child: const Column(
-                  children: [
-                    Text(
-                        'Communication cuisine',
+                  padding: const EdgeInsets.all(20),
+                  child: const Column(
+                    children: [
+                      Text(
+                        'Prendre un commande',
                         style: TextStyle(
-                            fontSize: 18,
-                            color: Color(0xFFFFEB99),
-                            fontFamily: 'LibreBaskerville'
+                          fontSize: 18,
+                          color: Color(0xFFFFEB99),
+                          fontFamily: 'LibreBaskerville'
                         )
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-
-          const SizedBox(height: 10),
-
-          Padding(
-            padding: const EdgeInsets.only(left: 10, right: 10),
-            child: GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => CommandeState(title: 'Commandes', token: widget.token),
+                      ),
+                    ],
                   ),
-                );
-              },
-              child: Container(
-                width: MediaQuery.of(context).size.width*0.9,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  border: Border.all(color: Color(0xFFFFEB99), width: 0.5),
-                  color: Color(0xFF292929),
-                ),
-                padding: const EdgeInsets.all(20),
-                child: const Column(
-                  children: [
-                    Text(
-                        'Voir les commandes',
-                        style: TextStyle(
-                            fontSize: 18,
-                            color: Color(0xFFFFEB99),
-                            fontFamily: 'LibreBaskerville'
-                        )
-                    ),
-                  ],
                 ),
               ),
             ),
-          ),
-          const SizedBox(height: 30),
-        ],
+
+            const SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.only(left: 10, right: 10),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Scanner(title: 'Scanner', token: widget.token),
+                    ),
+                  );
+                },
+                child: Container(
+                  width: MediaQuery.of(context).size.width*0.9,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(color: Color(0xFFFFEB99), width: 0.5),
+                    color: Color(0xFF292929),
+                  ),
+                  padding: const EdgeInsets.all(20),
+                  child: const Column(
+                    children: [
+                      Text(
+                        'Scanner un réservation',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Color(0xFFFFEB99),
+                          fontFamily: 'LibreBaskerville'
+                        )
+                      ),
+                    ],
+                  ),
+                )
+              )
+            ),
+
+            const SizedBox(height: 10),
+
+            Padding(
+              padding: const EdgeInsets.only(left: 10, right: 10),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Discussion(title: 'Discussion', token: widget.token),
+                    ),
+                  );
+                },
+                child: Container(
+                  width: MediaQuery.of(context).size.width*0.9,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(color: Color(0xFFFFEB99), width: 0.5),
+                    color: Color(0xFF292929),
+                  ),
+                  padding: const EdgeInsets.all(20),
+                  child: const Column(
+                    children: [
+                      Text(
+                          'Communication cuisine',
+                          style: TextStyle(
+                              fontSize: 18,
+                              color: Color(0xFFFFEB99),
+                              fontFamily: 'LibreBaskerville'
+                          )
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 10),
+
+            Padding(
+              padding: const EdgeInsets.only(left: 10, right: 10),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => CommandeState(title: 'Commandes', token: widget.token),
+                    ),
+                  );
+                },
+                child: Container(
+                  width: MediaQuery.of(context).size.width*0.9,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(color: Color(0xFFFFEB99), width: 0.5),
+                    color: Color(0xFF292929),
+                  ),
+                  padding: const EdgeInsets.all(20),
+                  child: const Column(
+                    children: [
+                      Text(
+                          'Voir les commandes',
+                          style: TextStyle(
+                              fontSize: 18,
+                              color: Color(0xFFFFEB99),
+                              fontFamily: 'LibreBaskerville'
+                          )
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 30),
+          ],
+        ),
       ),
     );
   }
